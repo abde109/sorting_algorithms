1b34ff89ee230cebed838e8f2f725a2f96eefa69
abde109 <abderrahimkhadri2@gmail.com>
2023-09-26 01:38:30 +0100

    betty status

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,33 +7,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (!list || !*list)
-        return;
+	if (!list || !*list)
+		return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        temp = current;
-        current = current->next;
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
 
-        while (temp && temp->prev && temp->prev->n > temp->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next)
-                temp->next->prev = temp->prev;
+		while (temp && temp->prev && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
 
-            temp->next = temp->prev;
-            temp->prev = temp->next->prev;
-            temp->next->prev = temp;
+			temp->next = temp->prev;
+			temp->prev = temp->next->prev;
+			temp->next->prev = temp;
 
-            if (temp->prev)
-                temp->prev->next = temp;
-            else
-                *list = temp;
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
 
-            print_list(*list);
-        }
-    }
+			print_list(*list);
+		}
+	}
 }
